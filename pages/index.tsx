ed074b626046b97863e6f2b1a3bee2660b8b7419
buddhiashen <108058142+buddhiashen@users.noreply.github.com>
2022-08-03 09:18:36 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,21 +6,16 @@ import Layout from '../components/Layout';
 import { getAllPosts } from '../lib/api';
 import { PostType } from '../types/post';
 
-
-
 type IndexProps = {
   posts: PostType[];
 };
 
-
 export const Index = ({ posts }: IndexProps): JSX.Element => {
   return (
     <Layout>
-      <h1>How are you fellas! 🤚 </h1>
-      <h1>Hello 👋 I'm Buddhi Ashen - curious fullstack student & Python noob based in Colombo, Sri-Lanka. This is my platform 
-        that i share my works, how i manage to solve problem when i am doing coding  and programming and many more....
-      </h1>
-      <p><a href="https://buddhi-ashen.github.io/buddhiashen/" target='_blank'>My portfolio</a></p>
+      <h1>How are you fellas!</h1>
+      <p>Hello ! , I&apos;m Buddhi Ashen - curious fullstack student and Python noob based in Colombo, Sri-Lanka. This is my platform that i share my works, how i manage to solve problem when i am doing coding  and programming and many more....</p>
+      <p><a href="https://buddhi-ashen.github.io/buddhiashen/">My portfolio</a></p>
       <p>i build this blog using a next js boilerplate template with:</p>
       <ul className="list-disc pl-4 my-6">
         <li>Next.js</li>
@@ -29,6 +24,7 @@ export const Index = ({ posts }: IndexProps): JSX.Element => {
         <li className="mt-2">Tailwind CSS</li>
       </ul>
 
+
       <a
         href="https://github.com/ChangoMan/nextjs-typescript-mdx-blog"
         className="inline-block px-7 py-3 rounded-md text-white dark:text-white bg-blue-600 hover:bg-blue-700 hover:text-white dark:hover:text-white"
@@ -36,18 +32,6 @@ export const Index = ({ posts }: IndexProps): JSX.Element => {
         Get the source code!
       </a>
 
-      <br /><br />
-      <p>enjoy the Blog while reading it and dont forget to share it, </p>
-      <p>contact me : 
-        <li>buddhiashen.info@gmail.com</li>
-        <li>https://twitter.com/buddhiashen4</li>
-        <li>https://www.facebook.com/buddhi.ashen.9</li>
-        <li>https://buddhi-ashen.github.io/buddhiashen/</li>
-      </p>
-      <br />
-<hr />
-<br />
-<h1>My dev blog</h1>
       {posts.map((post) => (
         <article key={post.slug} className="mt-12">
           <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
@@ -67,10 +51,7 @@ export const Index = ({ posts }: IndexProps): JSX.Element => {
             </Link>
           </p>
         </article>
-        
       ))}
-      <br />
-      <hr />
     </Layout>
   );
 };
